0cb7124ae5c5480a2a8ccf5221f17e8f39654fb3
msrittam <kumarmsrittam@gmail.com>
2024-01-05 12:31:05 -0800

    msrittam searchengine initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+import { SearchingDummyData } from './SearchingDummyData';
+import { useState } from 'react';
+
+export default function App() {
+  const [searchBarInput, setSearchBarInput]=useState()
+  return (
+    <div className="App">
+      <div className='AppHeaderWrapper'>
+        <h1>DataClaps</h1>
+        <button>Sign in</button>
+      </div>
+      <div className="AppSearchBarWrapper">
+        <svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg>
+        <input value={searchBarInput} onChange={(event)=>setSearchBarInput(event.target.value)}/>
+        <svg class="goxjub" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="#4285f4" d="m12 15c1.66 0 3-1.31 3-2.97v-7.02c0-1.66-1.34-3.01-3-3.01s-3 1.34-3 3.01v7.02c0 1.66 1.34 2.97 3 2.97z"></path><path fill="#34a853" d="m11 18.08h2v3.92h-2z"></path><path fill="#fbbc05" d="m7.05 16.87c-1.27-1.33-2.05-2.83-2.05-4.87h2c0 1.45 0.56 2.42 1.47 3.38v0.32l-1.15 1.18z"></path><path fill="#ea4335" d="m12 16.93a4.97 5.25 0 0 1 -3.54 -1.55l-1.41 1.49c1.26 1.34 3.02 2.13 4.95 2.13 3.87 0 6.99-2.92 6.99-7h-1.99c0 2.92-2.24 4.93-5 4.93z"></path></svg>
+        <svg class="Gdd5U" focusable="false" viewBox="0 0 192 192" xmlns="http://www.w3.org/2000/svg"><rect fill="none" height="192" width="192"></rect><g><circle fill="#34a853" cx="144.07" cy="144" r="16"></circle><circle fill="#4285f4" cx="96.07" cy="104" r="24"></circle><path fill="#ea4335" d="M24,135.2c0,18.11,14.69,32.8,32.8,32.8H96v-16l-40.1-0.1c-8.8,0-15.9-8.19-15.9-17.9v-18H24V135.2z"></path><path fill="#fbbc05" d="M168,72.8c0-18.11-14.69-32.8-32.8-32.8H116l20,16c8.8,0,16,8.29,16,18v30h16V72.8z"></path><path fill="#4285f4" d="M112,24l-32,0L68,40H56.8C38.69,40,24,54.69,24,72.8V92h16V74c0-9.71,7.2-18,16-18h80L112,24z"></path></g></svg>
+      </div>
+      <div className="AppSearchCategories">
+        <h3>All</h3>
+        <h3>Images</h3>
+        <h3>Videos</h3>
+        <h3>Shopping</h3>
+        <h3>News</h3>
+        <h3>Books</h3>
+        <h3>Maps</h3>
+        <h3>Flights</h3>
+        <h3>Finance</h3>
+        <h3>Search tools</h3>
+      </div>
+      {/* applied the search engine filter on below code */}
+      {SearchingDummyData.filter((item)=>{
+        if(searchBarInput){return item.toLowerCase().includes(searchBarInput.toLowerCase())}
+        else{return item}
+      }).map((dummyData)=>{
+      return(<div className="AppItemListWrapper">
+        <div className='AppItemHeadingBar'>
+          <svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"></path></svg>
+          <span>
+            <h5>DataClaps</h5>
+            <h5>www.DataClaps.com</h5>
+          </span>
+        </div>
+        <h3>{dummyData}</h3>
+        <h5>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h5>
+      </div>)})}
+    </div>
+  );
+}
+
